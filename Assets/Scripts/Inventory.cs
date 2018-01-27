2208117b9f55c1dd5261f52db7bc31f52e2cb8f7
Ryne Gipson <rgipson1@students.kennesaw.edu>
2018-01-27 13:23:53 -0500

    Lots of assets and initial work

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+
+namespace Assets.Scripts
+{
+    public enum Type { ferrium, resonite, chlorophate, neosteel, vibranium, photonite, unobtanium, adamantite, mythrill};
+    public class Inventory
+    {
+        int maxResources;
+        int maxBlips;
+        int currentBlips;
+        int currentResourceCount;
+        IDictionary<int, int> inv;
+        
+        public Inventory(int maxResources, int maxBlips)
+        {
+            this.maxResources = maxResources;
+            this.maxBlips = maxBlips;
+            currentBlips = maxBlips;
+            currentResourceCount = 0;
+            inv = new Dictionary<int, int>();
+            for (int i = 0; i <= 8; i++)
+            {
+                inv.Add(i, 0);
+            }
+        }
+
+        public bool AddResource(int itemType)
+        {
+            if (currentResourceCount == maxResources)
+                return false;
+            inv[itemType] = inv[itemType] + 1;
+            currentResourceCount++;
+            return true;
+        }
+
+        public bool AddBlip()
+        {
+            if (currentBlips == maxBlips)
+                return false;
+            currentBlips++;
+            return true;
+        }
+
+        public int GetBlips()
+        {
+            return currentBlips;
+        }
+
+        public int GetResourceCount(int resource)
+        {
+            return inv[resource];
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerProximityChecker.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerProximityChecker : MonoBehaviour {
+
+    public float player1Prox;
+    public float player2Prox;
+    public float triggerProx;
+    public Transform player1;
+    public Transform player2;
+
+	// Use this for initialization
+	void Start () {
+        triggerProx = 3f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        player1Prox = Vector3.Distance(transform.position, player1.transform.position);
+        player2Prox = Vector3.Distance(transform.position, player2.transform.position);
+        if (player1Prox < triggerProx)
+        {
+            print("GET PLAYER 1 AWAY!!!!");
+        }
+        if (player2Prox < triggerProx)
+        {
+            print("GET PLAYER 2 AWAY!!!!");
+        }
+    }
+}
